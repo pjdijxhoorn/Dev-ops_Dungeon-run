7016660b3b73692d91aaa894f9cee679d5651f5b
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2023-12-19 15:04:11 +0100

    <Fix> key-problem

--- Dungeon_run/settings.py
+++ Dungeon_run/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for Dungeon_run project.
 
@@ -9,8 +10,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lzz0dv^%_cf444c%sv)$9yrkpy+15@)tx96@8n1$&yz(76fste'
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'training'
 ]
 
 MIDDLEWARE = [
